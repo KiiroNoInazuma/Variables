582f7b304e6ddb311625d5002afa8c7a97a03a86
John Rey <adioskid@mail.ru>
2022-10-15 00:33:19 +0300

    Homework #1

--- src/Exercise_1.java
+++ src/Exercise_1.java
@@ -1,5 +1,18 @@
 public class Exercise_1 {
     public static void main(String[] args) {
-        
+
+        byte b = 7;
+        short s = 100;
+        int i = 25000;
+        long l = 5000000000L;
+        float f = 3.14f;
+        double d = 25.2332434;
+        char ch = 'a';
+        char chV = 97;
+        boolean bl = true;
+        boolean blV = false;
+        String str = "string";
+
+        System.out.println(b + "\n" + s + "\n" + i + "\n" + l + "\n" + f + "\n" + d + "\n" + ch + "\n" + chV + "\n" + bl + "\n" + bl + "\n" + blV + "\n" + str);
     }
 }
