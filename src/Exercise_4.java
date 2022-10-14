f26083c795c725a65c57a8e31e6490851341b4b2
John Rey <adioskid@mail.ru>
2022-10-15 02:04:08 +0300

    Homework #1

---
+++ src/Exercise_4.java
@@ -0,0 +1,19 @@
+public class Exercise_4 {
+    public static void main(String[] args) {
+
+        double weightTotal = 7;
+        double weightLoss = 250;
+        double weightLossS = 500;
+        double day, dayS, avg;
+
+        day = (weightTotal * 1000) / weightLoss;
+        dayS = (weightTotal * 1000) / weightLossS;
+        avg = (day + dayS) / 2;
+
+        System.out.println("При потери веса в " + weightLoss + " г. спортсмену понадобится " + day + " д.");
+        System.out.println("При потери веса в " + weightLossS + " г. спортсмену понадобится " + dayS + " д.");
+        System.out.println("В среднем потребуется " + avg + " д.");
+
+
+    }
+}
