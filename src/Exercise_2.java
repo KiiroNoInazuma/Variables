73895fe2958bb5390f83b8c710b414317e5ec257
John Rey <adioskid@mail.ru>
2022-10-15 00:50:41 +0300

    Homework #1

---
+++ src/Exercise_2.java
@@ -0,0 +1,13 @@
+public class Exercise_2 {
+    public static void main(String[] args) {
+
+        double boxBlue = 78.2;
+        double boxRed = 82.7;
+        double plus = boxBlue+boxRed;
+        double minus = boxRed-boxBlue;
+
+        System.out.println("Общий вес - "+plus+" кг");
+        System.out.println("Разница между 2-мя бойцами - "+minus+" кг");
+
+    }
+}
